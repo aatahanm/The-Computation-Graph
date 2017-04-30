436084241b9f7520fb02aba24700aa66a53523dd
Yaman Yagiz Tasbag <yytasbag@gmail.com>
2017-04-30 04:32:52 +0300

    rf

--- src/main/java/core/CEdge.java
+++ src/main/java/core/CEdge.java
@@ -10,8 +10,21 @@ import org.jgrapht.graph.DefaultEdge;
 public class CEdge extends DefaultEdge
 {
     private final int ID;
+    private int inputOrder;
+
     public CEdge()
     {
         ID = STATICS.getID();
+        inputOrder = 0;
+    }
+
+    int getOrder()
+    {
+        return inputOrder;
+    }
+
+    void setOrder ( int order)
+    {
+        inputOrder = order;
     }
 }
