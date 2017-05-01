a404ffd1fa4eaab0f34922cd1da2c092784461a0
mertcinar <qwe>
2017-05-01 19:06:53 +0300

    rf

--- src/main/java/core/CGraph.java
+++ src/main/java/core/CGraph.java
@@ -2,11 +2,6 @@ package core;
 
 import org.jgrapht.experimental.dag.DirectedAcyclicGraph;
 
-/**
- * {CGraph}
- * Author: Yaman Yağız Taşbağ
- * Version: {30.04.2017}
- */
 public class CGraph<CVertex, CEdge> extends DirectedAcyclicGraph
 {
     public CGraph(Class edgeClass) {
