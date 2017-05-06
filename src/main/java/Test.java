997a764f7e3e3e98e7f7aea2d9da4b30782a2e7f
Yaman Yagiz Tasbag <yytasbag@gmail.com>
2017-05-06 17:29:36 +0300

    rf

---
+++ src/main/java/Test.java
@@ -0,0 +1,14 @@
+import core.CGraph;
+
+/**
+ * {Test}
+ * Author: Yaman Yağız Taşbağ
+ * Version: {2.05.2017}
+ */
+public class Test
+{
+    public static void main ( String[] args)
+    {
+        CGraph c = new CGraph();
+    }
+}
