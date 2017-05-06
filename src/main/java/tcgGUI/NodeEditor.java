e52e4e4c11564fc5b5203c74d40b21e5e87460ef
Yaman Yagiz Tasbag <yytasbag@gmail.com>
2017-05-06 17:53:30 +0300

    rf

---
+++ src/main/java/tcgGUI/NodeEditor.java
@@ -0,0 +1,16 @@
+package tcgGUI;
+
+import javax.swing.*;
+
+/**
+ * {NodeEditor}
+ * Author: Yaman Yağız Taşbağ
+ * Version: {6.05.2017}
+ */
+public class NodeEditor extends JFrame
+{
+    public void calc()
+    {
+
+    }
+}
