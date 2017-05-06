a23627ba25e611ea90bda4b1c23f5a0c22e155a0
Yaman Yagiz Tasbag <yytasbag@gmail.com>
2017-05-06 18:04:33 +0300

    rf

--- src/main/java/tcgGUI/NodeEditor.java
+++ src/main/java/tcgGUI/NodeEditor.java
@@ -1,5 +1,10 @@
 package tcgGUI;
 
+import core.CGraph;
+import org.jgrapht.ListenableGraph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.ListenableDirectedGraph;
+
 import javax.swing.*;
 
 /**
@@ -12,8 +17,17 @@ public class NodeEditor extends JPanel
 
     public void init()
     {
+        // create a JGraphT graph
+        ListenableGraph g = new ListenableDirectedGraph( DefaultEdge.class );
 
+        // create a visualization using JGraph, via an adapter
+        JGraphModelAdapter m_jgAdapter = new JGraphModelAdapter(g);
+
+        CGraph jgraph = new CGraph( m_jgAdapter );
+
+        add(jgraph);
     }
+
     public void calc()
     {
 
