ec18e5b5d6629fe5d515ca39693da07dd86a3d5b
mertcinar <qwe>
2017-05-12 08:25:34 +0300

    rf

--- src/main/java/tcg/core/STATICS.java
+++ src/main/java/tcg/core/STATICS.java
@@ -12,6 +12,16 @@ public class STATICS {
     public static final int MULTIPLICATION_VERTEX = 4;
     public static final int OUTPUT_VERTEX = 5;
     public static final int SIGMOID_VERTEX = 6;
+    public static final int COS_VERTEX = 7;
+    public static final int COT_VERTEX = 8;
+    public static final int DIVISION_VERTEX = 9;
+    public static final int EXP_VERTEX = 10;
+    public static final int LOG_VERTEX = 11;
+    public static final int SIN_VERTEX = 12;
+    public static final int SUBTRACTION_VERTEX = 13;
+    public static final int TANH_VERTEX = 14;
+    public static final int TAN_VERTEX = 15;
+
     public static final int CGRAPH = 99;
 
 
