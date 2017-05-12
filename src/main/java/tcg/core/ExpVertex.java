1494f96477fbe0116780de230babf1dbb665ebd1
mertcinar <qwe>
2017-05-12 08:14:10 +0300

    rf

---
+++ src/main/java/tcg/core/ExpVertex.java
@@ -0,0 +1,33 @@
+package tcg.core;
+
+import java.util.ArrayList;
+
+/**
+ * Created by mertcinar on 12/05/2017.
+ */
+public class ExpVertex extends CVertex{
+
+    public ExpVertex() {
+        inpCount = 1;
+        super.input.add(0.0);
+    }
+
+    @Override
+    public ArrayList<Double> getDerivative(int ID) {
+        ArrayList<Double> d = new ArrayList<>();
+        d.add(Math.exp(super.input.get(0)));
+        return d;
+    }
+
+    @Override
+    ArrayList<Double> calcOutput() {
+        ArrayList<Double> al = new ArrayList<>();
+        al.add(Math.exp(super.input.get(0)));
+        return al;
+    }
+
+    public String toString()
+    {
+        return "exp(x0)";
+    }
+}
