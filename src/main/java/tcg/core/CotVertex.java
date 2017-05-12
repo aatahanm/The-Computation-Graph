88405c02f878f7f9d8a9451af2939f97e0b61319
mertcinar <qwe>
2017-05-12 08:17:56 +0300

    rf

---
+++ src/main/java/tcg/core/CotVertex.java
@@ -0,0 +1,33 @@
+package tcg.core;
+
+import java.util.ArrayList;
+
+/**
+ * Created by mertcinar on 12/05/2017.
+ */
+public class CotVertex extends CVertex{
+
+    public CotVertex() {
+        inpCount = 1;
+        super.input.add(0.0);
+    }
+
+    @Override
+    public ArrayList<Double> getDerivative(int ID) {
+        ArrayList<Double> d = new ArrayList<>();
+        d.add(-1.0/Math.pow(Math.sin(input.get(0)),2));
+        return d;
+    }
+
+    @Override
+    ArrayList<Double> calcOutput() {
+        ArrayList<Double> al = new ArrayList<>();
+        al.add(1.0/Math.tan(super.input.get(0)));
+        return al;
+    }
+
+    public String toString()
+    {
+        return "cot(x0)";
+    }
+}
